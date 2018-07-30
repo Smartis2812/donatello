1c08b95189ae9afbb9e12b449eabb500e414d462
Matthias Weingaertner <matt.weingaertner@gmail.com>
2018-07-30 20:08:47 +0200

    Adding Dependency Injection 💉

--- Donatello/Controllers/HomeController.cs
+++ Donatello/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Donatello.Services;
 using Donatello.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,17 +10,17 @@ namespace Donatello.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BoardService boardService;
+
+        public HomeController(BoardService boardService)
+        {
+            this.boardService = boardService;
+        }
+
         public IActionResult Index()
         {
-            var model = new BoardList();
+            BoardList model = boardService.ListBoards();
 
-            var board = new BoardList.Board();
-            board.Title = "Matt's Board";
-            model.Boards.Add(board);
-
-            var anotherBoard = new BoardList.Board();
-            anotherBoard.Title = "Another Board";
-            model.Boards.Add(anotherBoard);            
             return View(model);
         }
     }
---
+++ Donatello/Services/BoardService.cs
@@ -0,0 +1,25 @@
+﻿using Donatello.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Donatello.Services
+{
+    public class BoardService
+    {
+        public BoardList ListBoards()
+        {
+            var model = new BoardList();
+
+            var board = new BoardList.Board();
+            board.Title = "Matt's Board";
+            model.Boards.Add(board);
+
+            var anotherBoard = new BoardList.Board();
+            anotherBoard.Title = "Another Board";
+            model.Boards.Add(anotherBoard);
+            return model;
+        }
+    }
+}
