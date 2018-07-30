ca2403c89f5c418c954fd6263599e316ef3d41ee
Matthias Weingaertner <matt.weingaertner@gmail.com>
2018-07-30 19:41:48 +0200

    Initial commit and first feature 🔥

---
+++ Donatello/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Donatello.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Donatello.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new BoardList();
+
+            var board = new BoardList.Board();
+            board.Title = "Matt's Board";
+            model.Boards.Add(board);
+
+            var anotherBoard = new BoardList.Board();
+            anotherBoard.Title = "Another Board";
+            model.Boards.Add(anotherBoard);            
+            return View(model);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+﻿# Donatello - ASP.NET Core 2.1 Trello clone
+
+Just a simple trainings repo.
