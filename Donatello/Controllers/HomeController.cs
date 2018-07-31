e470e07974f58055f007c0e164bc5dd213bd0bb6
Matthias Weingaertner <matt.weingaertner@gmail.com>
2018-07-31 21:44:50 +0200

    XSRF and CSRF protection 🔒

--- Donatello/Controllers/HomeController.cs
+++ Donatello/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Donatello.Controllers
 {
+    [AutoValidateAntiforgeryToken]
     public class HomeController : Controller
     {
         private readonly BoardService boardService;
